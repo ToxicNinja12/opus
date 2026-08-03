163f017847dd5b4d85262b7583ee79ab1b3e988b
ToxicNinja12 <toxicninja1256@gmail.com>
2026-08-03 13:43:41 +0530

    Add social proof section

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Hero />
       <About />
       <Products />
-      {/* <Testimonials /> */}
+      <Testimonials />
       <Footer />
     </>
   );
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -2,21 +2,50 @@ import logo1 from "../assets/clients/logo-adnoc.png";
 import logo2 from "../assets/clients/logo-bmw.png";
 import logo3 from "../assets/clients/logo-gems.png";
 import logo4 from "../assets/clients/logo-ministry.png";
+import logo5 from "../assets/clients/logo-khda.png";
+import logo6 from "../assets/clients/logo-arla.png";
+import logo7 from "../assets/clients/logo-dubai-british-school.png";
+import logo8 from "../assets/clients/logo-fujifilm.png";
+import logo9 from "../assets/clients/logo-nakheel.png";
+import logo10 from "../assets/clients/logo-dana-gas.png";
+import logo11 from "../assets/clients/logo-lycee-francais.png";
+import logo12 from "../assets/clients/logo-red-sea-international.png";
+import logo13 from "../assets/clients/logo-miral-studio.png";
+import logo14 from "../assets/clients/logo-aquila-school.png";
 
 function Testimonials() {
-  const logos = [logo1, logo2, logo3, logo4];
+  const logos = [
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    logo5,
+    logo6,
+    logo7,
+    logo8,
+    logo9,
+    logo10,
+    logo11,
+    logo12,
+    logo13,
+    logo14,
+  ];
 
   return (
-    <section className="py-12">
-      <h2 className="font-lead text-sky-950 text-center text-xl mb-4">
-        Trusted by
+    <section className="text-sky-950 px-20 py-32">
+      <h2 className="font-lead text-center text-xl mb-10">
+        Trusted by 40+ leading companies
       </h2>
-      <div className="flex justify-center items-center gap-12 h-18">
+      <div className="flex flex-wrap justify-center items-center gap-12">
         {logos.map((logo) => (
           <img
             src={logo}
-            alt=""
-            className="h-full grayscale-100 brightness-125 contrast-50"
+            alt="Client logo"
+            className="h-18"
+            style={{
+              filter:
+                "contrast(25%) sepia(100%) hue-rotate(180deg) saturate(100%)",
+            }}
           />
         ))}
       </div>
