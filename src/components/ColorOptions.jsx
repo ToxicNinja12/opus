5bd214bb4363fec99d5601912b09f238be771d31
ToxicNinja12 <toxicninja1256@gmail.com>
2026-09-07 21:16:43 +0530

    Change color options component for laptop displays

--- src/components/ColorOptions.jsx
+++ src/components/ColorOptions.jsx
@@ -1,25 +1,36 @@
 import swatches1 from "../assets/products/acou-max/colors-1.jpg";
 import swatches2 from "../assets/products/acou-max/colors-2.jpg";
 import swatches3 from "../assets/products/acou-max/colors-3.jpg";
+import swatches4 from "../assets/products/acou-max/colors-4.jpg";
 
 function ColorOptions() {
   return (
     <div className="mb-8 mt-12">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-y-0">
+      {/* Mobile and Tablet: Original stacked layout */}
+      <div className="flex flex-col gap-6 lg:hidden">
         <img
           src={swatches1}
           alt="AcouMAX color swatches, page 1 of 3"
-          className="w-full object-contain order-1 lg:order-2"
+          className="w-full object-contain"
         />
         <img
           src={swatches2}
           alt="AcouMAX color swatches, page 2 of 3"
-          className="w-full object-contain order-2 lg:order-1"
+          className="w-full object-contain"
         />
         <img
           src={swatches3}
           alt="AcouMAX color swatches, page 3 of 3"
-          className="w-full object-contain order-3"
+          className="w-full object-contain"
+        />
+      </div>
+
+      {/* Laptop and Desktop: Single combined image */}
+      <div className="hidden lg:block">
+        <img
+          src={swatches4}
+          alt="AcouMAX all color swatches"
+          className="w-full object-contain"
         />
       </div>
     </div>
