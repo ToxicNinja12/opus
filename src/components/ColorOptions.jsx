f135277e178b2b129dc1848d3e25e2f1f07b1c67
ToxicNinja12 <toxicninja1256@gmail.com>
2026-09-07 17:38:36 +0530

    Display colors side by side in ColorOptions

--- src/components/ColorOptions.jsx
+++ src/components/ColorOptions.jsx
@@ -5,21 +5,21 @@ import swatches3 from "../assets/products/acou-max/colors-3.jpg";
 function ColorOptions() {
   return (
     <div className="mb-8 mt-12">
-      <div className="flex flex-col gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-y-0">
         <img
           src={swatches1}
           alt="AcouMAX color swatches, page 1 of 3"
-          className="w-full object-contain"
+          className="w-full object-contain order-1 lg:order-2"
         />
         <img
           src={swatches2}
           alt="AcouMAX color swatches, page 2 of 3"
-          className="w-full object-contain"
+          className="w-full object-contain order-2 lg:order-1"
         />
         <img
           src={swatches3}
           alt="AcouMAX color swatches, page 3 of 3"
-          className="w-full object-contain"
+          className="w-full object-contain order-3"
         />
       </div>
     </div>
